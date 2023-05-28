31900efc37d977663d19c97ab0598c786461ec15
Steven Velozo <steven@velozo.com>
2023-05-28 08:39:10 -0700

    Added new packages

--- modules/Include-Retold-Module-List.sh
+++ modules/Include-Retold-Module-List.sh
@@ -2,14 +2,14 @@ Retold-Module-List.sh
 
 echo "### Building list sof modules..."
 
-repositoriesFable=("fable" "fable-log" "fable-settings" "fable-uuid")
+repositoriesFable=("fable" "fable-log" "fable-settings" "fable-uuid" "fable-serviceproviderbase")
 
 repositoriesMeadow=("stricture" "foxhound" "meadow" "meadow-endpoints" "retold-data-service")
 
 repositoriesOrator=("orator" "orator-serviceserver-restify" "tidings")
 
-repositoriesPict=("pict" "cryptbrau" "informary")
+repositoriesPict=("pict" "cryptbrau" "informary" "pict-chance")
 
-repositoriesUtility=("precedent" "manyfest" "choreographic" "elucidator" "cachetrax" "cumulation" "merquerial" "quantifier")
+repositoriesUtility=("precedent" "manyfest" "choreographic" "elucidator" "cachetrax" "cumulation" "merquerial" "quantifier" "quackage")
 
 echo "### ... Module lists built!"
--- modules/Status.sh
+++ modules/Status.sh
@@ -7,14 +7,15 @@ echo "Checking out Retold modules into: [$(pwd)/..."
 # This function checks the status of a repository
 status_repository()
 {
-	echo "###--> attempting to get the status of the repository for $2 in $1"
+#	echo "###--> attempting to get the status of the repository for $2 in $1"
 	CWD=$(pwd)
 	if [ -d "$CWD/$1/$2" ]
 	then
-		echo "     # A $2 source directory exists in $1 -- chcking status...."
+#		echo "     # A $2 source directory exists in $1 -- chcking status...."
 		cd "$CWD/$1/$2"
 		git status
 		echo "     ..."
+		echo " "
 		cd "../.."
 	else
 		echo "     # The $2 source directory does not exist in $1 -- maybe you need to run the Checkout script?"
