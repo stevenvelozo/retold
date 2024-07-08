7fda539df3ab86ddb2816e89d54618d9164d425a
Steven Velozo <steven@velozo.com>
2024-07-08 07:24:29 -0700

    Added and cleaned up module list

--- modules/Include-Retold-Module-List.sh
+++ modules/Include-Retold-Module-List.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "### Building list sof modules..."
+echo "### Building list of modules..."
 
 repositoriesFable=("fable" "fable-log" "fable-settings" "fable-uuid" "fable-serviceproviderbase" "fable-log-logger-bunyan")
 
-repositoriesMeadow=("stricture" "foxhound" "meadow" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "retold-data-service" "retold-harness" "meadow-integration")
+repositoriesMeadow=("stricture" "foxhound" "meadow" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "meadow-connection-sqlite" "retold-data-service" "retold-harness" "meadow-integration")
 
 repositoriesOrator=("orator" "orator-serviceserver-restify" "tidings")
 
-repositoriesPict=("pict" "pict-view" "pict-application" "cryptbrau" "informary" "pict-service-commandlineutility" "pict-chance" "pict-section-content" "pict-section-form" "pict-section-tuigrid" "pict-router")
+repositoriesPict=("pict" "pict-template" "pict-view" "pict-provider" "pict-application" "pict-panel" "cryptbrau" "informary" "pict-service-commandlineutility" "pict-chance" "pict-section-content" "pict-section-form" "pict-section-tuigrid" "pict-router")
 
 repositoriesUtility=("indoctrinate" "precedent" "manyfest" "choreographic" "elucidator" "cachetrax" "cumulation" "merquerial" "quantifier" "quackage" "pict-cruisecontrol")
 
