f15e2fadc3b76b1395af2a2c03842e8c5f982959
Steven Velozo <steven@velozo.com>
2024-09-18 12:38:01 -0700

    Added graph client to tooling list

--- modules/Include-Retold-Module-List.sh
+++ modules/Include-Retold-Module-List.sh
@@ -4,12 +4,12 @@ echo "### Building list of modules..."
 
 repositoriesFable=("fable" "fable-log" "fable-settings" "fable-uuid" "fable-serviceproviderbase" "fable-log-logger-bunyan")
 
-repositoriesMeadow=("stricture" "foxhound" "meadow" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "meadow-connection-sqlite" "retold-data-service" "retold-harness" "meadow-integration")
+repositoriesMeadow=("stricture" "foxhound" "meadow" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "meadow-connection-sqlite" "retold-data-service" "retold-harness" "meadow-integration" "meadow-graph-client")
 
 repositoriesOrator=("orator" "orator-serviceserver-restify" "tidings")
 
 repositoriesPict=("pict" "pict-template" "pict-view" "pict-provider" "pict-application" "pict-panel" "cryptbrau" "informary" "pict-service-commandlineutility" "pict-chance" "pict-section-content" "pict-section-form" "pict-section-tuigrid" "pict-router")
 
-repositoriesUtility=("indoctrinate" "precedent" "manyfest" "choreographic" "elucidator" "cachetrax" "cumulation" "merquerial" "quantifier" "quackage" "pict-cruisecontrol")
+repositoriesUtility=("indoctrinate" "precedent" "manyfest" "choreographic" "elucidator" "cachetrax" "cumulation" "merquerial" "quackage" "pict-cruisecontrol")
 
 echo "### ... Module lists built!"
