15728c1c10269e0a94f1729f7eb28581370515bc
Steven Velozo <steven@velozo.com>
2025-05-31 11:23:38 -0700

    Adding new modules to megalist

--- modules/Include-Retold-Module-List.sh
+++ modules/Include-Retold-Module-List.sh
@@ -4,12 +4,12 @@ echo "### Building list of modules..."
 
 repositoriesFable=("fable" "fable-log" "fable-settings" "fable-uuid" "fable-serviceproviderbase" "fable-log-logger-bunyan")
 
-repositoriesMeadow=("stricture" "foxhound" "meadow" "parime" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "meadow-connection-sqlite" "retold-data-service" "retold-harness" "meadow-integration" "meadow-graph-client")
+repositoriesMeadow=("stricture" "foxhound" "bibliograph" "meadow" "parime" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "meadow-connection-sqlite" "retold-data-service" "retold-harness" "meadow-integration" "meadow-graph-client")
 
-repositoriesOrator=("orator" "orator-serviceserver-restify" "orator-static-server" "orator-http-proxy" "tidings")
+repositoriesOrator=("orator" "orator-serviceserver-restify" "orator-static-server" "orator-http-proxy" "tidings" "orator-endpoint")
 
-repositoriesPict=("pict" "pict-template" "pict-view" "pict-provider" "pict-application" "pict-panel" "cryptbrau" "informary" "pict-service-commandlineutility" "pict-chance" "pict-section-content" "pict-section-entitymanagement" "pict-section-form" "pict-section-tuigrid" "pict-router")
+repositoriesPict=("pict" "pict-template" "pict-view" "pict-provider" "pict-application" "pict-panel" "cryptbrau" "informary" "pict-service-commandlineutility" "pict-section-recordset" "pict-section-content" "pict-section-form" "pict-section-tuigrid" "pict-router")
 
-repositoriesUtility=("indoctrinate" "manyfest" "choreographic" "quackage" "pict-cruisecontrol")
+repositoriesUtility=("indoctrinate" "manyfest" "choreographic" "quackage" "ultravisor")
 
 echo "### ... Module lists built!"
