fa323cfebe2a194c1aa11f3f94551bcce1c2055c
Steven Velozo <steven@velozo.com>
2026-02-15 14:03:31 -0800

    New documentation

--- modules/Include-Retold-Module-List.sh
+++ modules/Include-Retold-Module-List.sh
@@ -4,12 +4,12 @@ echo "### Building list of modules..."
 
 repositoriesFable=("fable" "fable-log" "fable-settings" "fable-uuid" "fable-serviceproviderbase" "fable-log-logger-bunyan")
 
-repositoriesMeadow=("stricture" "foxhound" "bibliograph" "meadow" "parime" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "meadow-connection-sqlite" "retold-data-service" "retold-harness" "meadow-integration" "meadow-graph-client")
+repositoriesMeadow=("stricture" "foxhound" "bibliograph" "meadow" "parime" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "meadow-connection-sqlite" "retold-data-service" "retold-harness" "meadow-integration")
 
 repositoriesOrator=("orator" "orator-serviceserver-restify" "orator-static-server" "orator-http-proxy" "tidings" "orator-endpoint" "orator-conversion")
 
 repositoriesPict=("pict" "pict-template" "pict-view" "pict-provider" "pict-application" "pict-panel" "pict-nonlinearconfig" "pict-section-flow" "pict-docuserve" "cryptbrau" "informary" "pict-service-commandlineutility" "pict-section-recordset" "pict-section-content" "pict-section-form" "pict-section-tuigrid" "pict-router" "pict-serviceproviderbase" "pict-terminalui")
 
-repositoriesUtility=("indoctrinate" "manyfest" "choreographic" "quackage" "ultravisor")
+repositoriesUtility=("indoctrinate" "manyfest" "quackage" "ultravisor")
 
 echo "### ... Module lists built!"
