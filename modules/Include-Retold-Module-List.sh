b857511e47e5b9956504c655580c96e14b68a1a9
Steven Velozo <steven@velozo.com>
2026-03-12 08:06:53 -0700

    Fixed module list

--- modules/Include-Retold-Module-List.sh
+++ modules/Include-Retold-Module-List.sh
@@ -6,7 +6,7 @@ repositoriesFable=("fable" "fable-log" "fable-settings" "fable-uuid" "fable-serv
 
 repositoriesMeadow=("stricture" "foxhound" "bibliograph" "bibliograph-storage-meadow" "meadow" "parime" "meadow-endpoints" "meadow-connection-mysql" "meadow-connection-mssql" "meadow-connection-sqlite" "meadow-connection-sqlite-browser" "meadow-connection-postgresql" "meadow-connection-mongodb" "meadow-connection-dgraph" "meadow-connection-solr" "meadow-connection-rocksdb" "meadow-graph-client" "retold-data-service" "retold-harness" "retold-harness-consistency-proxy" "meadow-integration" "meadow-migrationmanager" "meadow-provider-offline")
 
-repositoriesOrator=("orator" "orator-serviceserver-restify" "orator-static-server" "orator-http-proxy" "orator-endpoint" "tidings" "orator-conversion")
+repositoriesOrator=("orator" "orator-serviceserver-restify" "orator-static-server" "orator-http-proxy" "orator-endpoint" "tidings" "orator-conversion" "orator-authentication")
 
 repositoriesPict=("pict" "pict-template" "pict-view" "pict-provider" "pict-application" "pict-panel" "pict-nonlinearconfig" "pict-section-flow" "pict-docuserve" "cryptbrau" "informary" "pict-service-commandlineutility" "pict-section-openseadragon" "pict-section-recordset" "pict-section-code" "pict-section-content" "pict-section-objecteditor" "pict-section-formeditor" "pict-section-form" "pict-section-tuigrid" "pict-router" "pict-serviceproviderbase" "pict-terminalui" "pict-sessionmanager" "pict-section-markdowneditor")
 
